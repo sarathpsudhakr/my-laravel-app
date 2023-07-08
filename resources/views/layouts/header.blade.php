6681256fce3f97abd89259a18ab101017e9e805f
Sarath <sarathpsudhakr@gmail.com>
2023-07-08 00:53:00 -0600

    Added route names and urls.

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -23,8 +23,8 @@
 				</div>
 				<div class="col-md-6">
 					<div class="right-element">
-						<a href="#" class="user-account for-buy"><i class="icon icon-user"></i><span>Account</span></a>
-						<a href="#" class="cart for-buy"><i class="icon icon-clipboard"></i><span>Cart:(0 $)</span></a>
+						<a href="{{ url('account') }}" class="user-account for-buy"><i class="icon icon-user"></i><span>Account</span></a>
+						<a href="{{ url('cart') }}" class="cart for-buy"><i class="icon icon-clipboard"></i><span>Cart:(0 $)</span></a>
 
 						<div class="action-menu">
 
@@ -51,7 +51,7 @@
 
 				<div class="col-md-2">
 					<div class="main-logo">
-						<a href="index.html"><img src="images/main-logo.png" alt="logo"></a>
+						<a href="{{ url('home') }}"><img src="{{ asset('images/main-logo.png') }}" alt="logo"></a>
 					</div>
 
 				</div>
@@ -62,26 +62,10 @@
 						<div class="main-menu stellarnav">
 							<ul class="menu-list">
 								<li class="menu-item active"><a href="#home" data-effect="Home">Home</a></li>
-								<li class="menu-item"><a href="#about" class="nav-link" data-effect="About">About</a></li>
-								<li class="menu-item has-sub">
-									<a href="#pages" class="nav-link" data-effect="Pages">Pages</a>
-
-									<ul>
-								        <li class="active"><a href="index.html">Home</a></li>
-								        <li><a href="about.html">About</a></li>
-								        <li><a href="styles.html">Styles</a></li>
-								        <li><a href="blog.html">Blog</a></li>
-								        <li><a href="single-post.html">Post Single</a></li>
-								        <li><a href="shop.html">Our Store</a></li>
-								        <li><a href="single-product.html">Product Single</a></li>
-								        <li><a href="contact.html">Contact</a></li>
-								        <li><a href="thank-you.html">Thank You</a></li>
-								     </ul>
-
-								</li>
-								<li class="menu-item"><a href="#popular-books" class="nav-link" data-effect="Shop">Shop</a></li>
-								<li class="menu-item"><a href="#latest-blog" class="nav-link" data-effect="Articles">Articles</a></li>
-								<li class="menu-item"><a href="#contact" class="nav-link" data-effect="Contact">Contact</a></li>
+								<li class="menu-item"><a href="{{ url('about') }}" class="nav-link" data-effect="About">About</a></li>
+								<li class="menu-item"><a href="{{ url('books') }}" class="nav-link" data-effect="Shop">Books</a></li>
+								<li class="menu-item"><a href="{{ url('blog') }}" class="nav-link" data-effect="Blog">Blog</a></li>
+								<li class="menu-item"><a href="{{ url('contact') }}" class="nav-link" data-effect="Contact">Contact</a></li>
 							</ul>
 
 							<div class="hamburger">
--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', 'HomeController@home');
-Route::get('/about', 'AboutController@about');
-Route::get('/blog', 'BlogController@blog');
-Route::get('/contact', 'ContactController@contact');
-Route::get('/cart', 'CartController@cart');
-Route::get('/books', 'BookController@books');
-Route::get('/view-book/{id}', 'BookController@viewBook');
-Route::get('/account', 'AccountController@account');
-Route::get('/search', 'SearchController@search');
+Route::get('home', 'HomeController@home')->name('home');
+Route::get('about', 'AboutController@about')->name('about');
+Route::get('blog', 'BlogController@blog')->name('blog');
+Route::get('contact', 'ContactController@contact')->name('contact');
+Route::get('cart', 'CartController@cart')->name('cart');
+Route::get('books', 'BookController@books')->name('books');
+Route::get('view-book/{id}', 'BookController@viewBook')->name('view.book');
+Route::get('account', 'AccountController@account')->name('account');
+Route::get('search', 'SearchController@search')->name('search');
